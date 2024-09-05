b6a7c6b50d330edba861fb5b6b3a054d4606c68e
zdogan <ismizelal@gmail.com>
2024-09-05 08:02:48 -0500

    add a comment to each block

--- main.tf
+++ main.tf
@@ -12,11 +12,14 @@ terraform {
   }
 }
 
+# the tls_private_key resource from the tls provider to generate RSA 4096-bit SSH keys.
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+#This function reads a template file (cloud-config.yaml.tpl) located in the module’s path and replaces the placeholders with the provided values.
+#Use the local_file resource from the local provider to generate cloud-config.yaml by rendering cloud-config.yaml.tpl and filling placeholders with the generated SSH keys. 
 resource "local_file" "cloud_config" {
   content  = templatefile("${path.module}/cloud-config.yaml.tpl", {
     public_key  = tls_private_key.ssh_key.public_key_openssh
